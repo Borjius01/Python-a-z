5c812668c68f69c35cca7d7bb1e4b43d24b82548
Borjius01 <borjiusbar@gmail.com>
2025-04-24 09:25:07 +0200

    añadiendo función input a main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 def main():
-    print("Hola desde el script principal!")
+    print("Hola desde el script principal!\n")
+
+    nombre = input("¿Cómo te llamas? ")
+    edad = input("¿Cuántos años tienes? ")
+    
+    print(f"\nEncantado, {nombre}. Tienes {edad} años.")
 
 if __name__ == "__main__":
     main()
